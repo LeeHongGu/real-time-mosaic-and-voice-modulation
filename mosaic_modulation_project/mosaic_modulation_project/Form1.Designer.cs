13b001cb8cda680a070df87ee359207e4185bc45
LeeHongGu <33251178+LeeHongGu@users.noreply.github.com>
2021-05-18 21:58:22 +0900

    second
    second dll and mosaic

--- mosaic_modulation_project/mosaic_modulation_project/Form1.Designer.cs
+++ mosaic_modulation_project/mosaic_modulation_project/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace mosaic_modulation_project
+namespace OpenCV_test1
 {
     partial class Form1
     {
@@ -30,12 +30,66 @@ namespace mosaic_modulation_project
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxIpl1
+            // 
+            this.pictureBoxIpl1.Location = new System.Drawing.Point(12, 32);
+            this.pictureBoxIpl1.Name = "pictureBoxIpl1";
+            this.pictureBoxIpl1.Size = new System.Drawing.Size(456, 340);
+            this.pictureBoxIpl1.TabIndex = 0;
+            this.pictureBoxIpl1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(539, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 136);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "예외처리";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(539, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(230, 136);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "음성변조";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(831, 406);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxIpl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- mosaic_modulation_project/mosaic_modulation_project/Form1.cs
+++ mosaic_modulation_project/mosaic_modulation_project/Form1.cs
@@ -7,14 +7,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
 
-namespace mosaic_modulation_project
+namespace OpenCV_test1
 {
     public partial class Form1 : Form
     {
+        VideoCapture video;
+        Mat frame = new Mat();
+        //int rate = 15;
+        //int x, y, w, h = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                video = new VideoCapture(0);
+                video.FrameWidth = 640;
+                video.FrameHeight = 480;
+            }
+            catch
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int sleepTime = (int)Math.Round(1000 / video.Fps);
+
+            String filenameFaceCascade = "haarcascade_frontalface_alt.xml";
+            CascadeClassifier faceCascade = new CascadeClassifier();
+
+            if (!faceCascade.Load(filenameFaceCascade))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            video.Read(frame);
+
+            // detect
+            Rect[] faces = faceCascade.DetectMultiScale(frame);
+            Console.WriteLine(faces.Length);
+
+
+            foreach (var item in faces)
+            {
+                //OpenCvSharp.Point lb = new OpenCvSharp.Point(item.X + item.Width, item.Y + item.Height);
+                //OpenCvSharp.Point tr = new OpenCvSharp.Point(item.X, item.Y);
+                Mat mosaic_frame = new Mat(frame, new Rect(item.X, item.Y, item.Width, item.Height));
+                Mat temp_frame = new Mat();
+
+                Cv2.Resize(mosaic_frame, temp_frame, new OpenCvSharp.Size(item.Width / 10, item.Height / 10));
+                Cv2.Resize(temp_frame, mosaic_frame, new OpenCvSharp.Size(item.Width, item.Height));
+
+                //Cv2.Rectangle(frame, item, Scalar.Red, 0); // add rectangle to the image
+
+                Console.WriteLine("faces : " + item);
+            }
+
+
+
+            // display
+            pictureBoxIpl1.ImageIpl = frame;
+
+            Cv2.WaitKey(sleepTime);
+            video.Release();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String filenameFaceCascade = "haarcascade_frontalface_alt.xml";
+            CascadeClassifier faceCascade = new CascadeClassifier();
+
+            if (!faceCascade.Load(filenameFaceCascade))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            //faceCascade.DetectMultiScale
         }
     }
 }
