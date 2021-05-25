954150447fbecdf4cb76749716e862247ec6c2d7
LeeHongGu <33251178+LeeHongGu@users.noreply.github.com>
2021-05-25 19:49:29 +0900

    voice modulation
    pitch_shift

--- audioMatlab/audio.m
+++ audioMatlab/audio.m
@@ -1,37 +1,18 @@
-function obj = audio(SampleRate,NumChannels,ID)
-    %frameLength = 1024;
+function obj = audio()
     deviceReader = audioDeviceReader;
     fileWriter = dsp.AudioFileWriter('output.wav','SampleRate',deviceReader.SampleRate);
     deviceWriter = audioDeviceWriter('SampleRate',deviceReader.SampleRate);
-    fileReader = dsp.AudioFileReader('output.wav');
+    %fileReader = dsp.AudioFileReader('output.wav');
     process = @(x) x.*5;
     disp('start')
     
     while 1
-        tic
-        while toc < 5
-            disp('start2')
-            mySignal = deviceReader();
-            %mySignal = fileReader();
-            myProcessedSignal = process(mySignal);
-            %deviceWriter(myProcessedSignal);
-            fileWriter(myProcessedSignal);
-            %deviceWriter(fileReader());
-            % tic
-        end
-        
-        while ~isDone(fileReader)
-            disp('start3')
-            mySignal = fileReader();
-            myProcessedSignal = process(mySignal);
-            deviceWriter(myProcessedSignal);
-        end
-%     while ~isDone(fileReader)
-%         mySignal = fileReader();
-%         myProcessedSignal = process(mySignal);
-%         deviceWriter(myProcessedSignal);
-%     end
+        mySignal = deviceReader();
+        myProcessedSignal = process(mySignal);
+        deviceWriter(myProcessedSignal);
+        fileWriter(myProcessedSignal); 
     end
+    
     disp('end')
     release(deviceReader)
     release(deviceWriter)
